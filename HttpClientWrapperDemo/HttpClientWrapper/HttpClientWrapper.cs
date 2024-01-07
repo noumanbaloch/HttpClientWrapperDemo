8525b421dee21f55d4811cfe53c4cf5b6a50fd4e
Nouman Baloch <nomibaloch775@hotmail.com>
2024-01-07 16:26:31 +0500

    adding httpclientwrapper

---
+++ HttpClientWrapperDemo/HttpClientWrapper/HttpClientWrapper.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace HttpClientWrapperDemo.HttpClientWrapper;
+
+public class HttpClientWrapper : IHttpClientWrapper
+{
+    private readonly HttpClient _httpClient;
+
+    public HttpClientWrapper(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<TResponse?> GetAsync<TResponse, TRequest>(string url, TRequest? request = default)
+    {
+        string queryString = request is null ? "" : $"?{Uri.EscapeDataString(request.ToString() ?? string.Empty)}";
+        HttpResponseMessage response = await _httpClient.GetAsync($"{url}{queryString}");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpRequestException($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+        }
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        return JsonConvert.DeserializeObject<TResponse>(responseBody);
+    }
+
+    public async Task<TResponse?> PostAsync<TResponse, TRequest>(string url, TRequest request, string contentType)
+    {
+        StringContent stringContent = new(JsonConvert.SerializeObject(request), Encoding.UTF8, contentType);
+
+        HttpResponseMessage response = await _httpClient.PostAsync(url, stringContent);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpRequestException($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+        }
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        return JsonConvert.DeserializeObject<TResponse>(responseBody);
+    }
+}
---
+++ HttpClientWrapperDemo/HttpClientWrapper/IHttpClientWrapper.cs
@@ -0,0 +1,7 @@
+﻿namespace HttpClientWrapperDemo.HttpClientWrapper;
+
+public interface IHttpClientWrapper
+{
+    Task<TResponse?> GetAsync<TResponse, TRequest>(string url, TRequest? request = default);
+    Task<TResponse?> PostAsync<TResponse, TRequest>(string url, TRequest request, string contentType);
+}
