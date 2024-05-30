88233998066f408fdf58de3f6f44e7e459d98b18
Nouman Baloch <nomibaloch775@hotmail.com>
2024-05-30 08:18:43 +0500

    improvements

--- HttpClientWrapperDemo/HttpClientWrapper/IHttpClientWrapper.cs
+++ HttpClientWrapperDemo/HttpClientWrapper/IHttpClientWrapper.cs
@@ -1,7 +1,9 @@
-﻿namespace HttpClientWrapperDemo.HttpClientWrapper;
+﻿using System.Net.Http.Headers;
+
+namespace HttpClientWrapperDemo.HttpClientWrapper;
 
 public interface IHttpClientWrapper
 {
-    Task<TResponse?> GetAsync<TResponse, TRequest>(string url, TRequest? request = default);
-    Task<TResponse?> PostAsync<TResponse, TRequest>(string url, TRequest request, string contentType);
+    Task<TResponse?> GetAsync<TResponse, TRequest>(string uri, AuthenticationHeaderValue? authenticationHeader = default, IEnumerable<KeyValuePair<string, string>>? customHeaders = default, TRequest? request = default);
+    Task<TResponse?> PostAsync<TResponse, TRequest>(string uri, AuthenticationHeaderValue? authenticationHeader = default, IEnumerable<KeyValuePair<string, string>>? customHeaders = default, TRequest? request = default);
 }
